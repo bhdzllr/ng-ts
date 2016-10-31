5c9470dd097edf57000e685ab1aedd218cd14345
bhdzllr <bhdzllr@users.noreply.github.com>
2016-10-31 11:15:54 +0100

    Styling

--- dist/js/components/list/list.js
+++ dist/js/components/list/list.js
@@ -1,20 +1,27 @@
 /// <reference path='../../../typings/index.d.ts' />
 'use strict';
 var ListCtrl = (function () {
-    function ListCtrl(api, filter) {
+    function ListCtrl(api) {
         var _this = this;
         this.api = api;
-        this.filter = filter;
-        this.data = [];
+        this.data = [{
+                'id': 0,
+                'title': 'Loading pull requests...',
+                'state': 'none',
+                'number': 0,
+                'created': 'loading...',
+                'url': '#'
+            }];
         // Type? Eigentlich promise.
         api.getData(100).then(function (response) {
+            _this.data = [];
             for (var i in response.data) {
                 _this.data.push({
                     'id': response.data[i].id,
                     'title': response.data[i].title,
                     'state': response.data[i].state,
                     'number': response.data[i].number,
-                    'created': response.data[i].created_at,
+                    'created': response.data[i].created_at.replace(/[TZ]/g, ' '),
                     'url': response.data[i].html_url
                 });
             }
--- ts/components/list/list.ts
+++ ts/components/list/list.ts
@@ -6,22 +6,30 @@ class ListCtrl {
 
 	static $inject = ['githubAPI'];
 
-	public data: IRecord[] = [];
+	public data: IRecord[] = [{
+		'id':      0,
+		'title':   'Loading pull requests...',
+		'state':   'none',
+		'number':  0,
+		'created': 'loading...',
+		'url':     '#'
+	}];
 
 	constructor(
-		private api: GitHubAPI,
-		private filter: ListFilter
+		private api: GitHubAPI
 	) {
 		// Type? Eigentlich promise.
 		api.getData(100).then((response: any) => {
+			this.data = [];
+
 			for (var i in response.data) {
 				this.data.push({
-					'id':        response.data[i].id,
-					'title':     response.data[i].title,
-					'state':     response.data[i].state,
-					'number':    response.data[i].number,
-					'created':   response.data[i].created_at,
-					'url':       response.data[i].html_url
+					'id':      response.data[i].id,
+					'title':   response.data[i].title,
+					'state':   response.data[i].state,
+					'number':  response.data[i].number,
+					'created': response.data[i].created_at.replace(/[TZ]/g, ' '),
+					'url':     response.data[i].html_url
 				});
 			}
 		});
